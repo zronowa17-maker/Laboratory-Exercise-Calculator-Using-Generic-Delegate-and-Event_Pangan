55620d38827f7153324eee5e6b2b24b1f8f7e212
zronowa17-maker <zronowa17@gmail.com>
2025-08-21 20:40:53 +0800

    declare a generic delegate

---
+++ CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    internal class CalculatorClass
+    {
+        public delegate T Information<T>(T arg1, T arg2);
+
+    }
+}
