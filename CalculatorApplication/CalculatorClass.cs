885b82c86f32a55cabec54ddd2da5fb6ce0bb9cb
zronowa17-maker <zronowa17@gmail.com>
2025-08-21 21:40:35 +0800

    add Getproduct and GetQuotient

--- CalculatorApplication/CalculatorClass.cs
+++ CalculatorApplication/CalculatorClass.cs
@@ -25,6 +25,18 @@ namespace CalculatorApplication
         {
             return num1 - num2;
         }
+        public double GetProduct(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double GetQuotient(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return num1 / num2;
+        }
         private Information<double> calculateEvent;
 
         public event Information<double> CalculateEvent
